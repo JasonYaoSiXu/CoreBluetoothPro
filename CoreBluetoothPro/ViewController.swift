fb65cfd1f57f55e2e83f10935d93274c9ce225d1
yaosixu <ysx199309173312@gmail.com>
2016-11-22 22:22:57 +0800

    进一步完善将ios设备设置为一台beacon设备

--- CoreBluetoothPro/ViewController.swift
+++ CoreBluetoothPro/ViewController.swift
@@ -11,10 +11,20 @@ import CoreBluetooth
 import CoreLocation
 //6D810169-5797-4EF2-B141-554360C4086E
 class ViewController: UIViewController {
+    
+    var coreBeaconRegion: CLBeaconRegion!
+    var peripheralManager: CBPeripheralManager!
+    var beaconPeripheralData: NSDictionary!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        useBlueTooth()
+        let uuidStr = "E16D436A-339B-4979-BD15-76878BA1A518"
+        guard let proximityUUID: UUID = UUID(uuidString: uuidStr) else {
+            return
+        }
+        coreBeaconRegion = CLBeaconRegion(proximityUUID: proximityUUID, major: 1, minor: 1, identifier: "yaosixu")
+        beaconPeripheralData = coreBeaconRegion.peripheralData(withMeasuredPower: nil)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,24 +33,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func useBlueTooth() {
-        guard let proximityUUID: UUID = UUID(uuidString: "6D810169-5797-4EF2-B141-554360C4086E") else {
-            return
-        }
-        
-        let coreBeaconRegion = CLBeaconRegion(proximityUUID: proximityUUID, identifier: "yaosixuBluetooth")
-        let beaconPeripheralData: NSDictionary = coreBeaconRegion.peripheralData(withMeasuredPower: nil)
-        let peripheralManager: CBPeripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
-        peripheralManager.startAdvertising(beaconPeripheralData as? [String: Any])
+    @IBAction func tapAction(_ sender: UIButton) {
+//        beaconPeripheralData = coreBeaconRegion.peripheralData(withMeasuredPower: nil)
+//        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
     }
-
 }
 
 extension ViewController: CBPeripheralManagerDelegate {
     
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         self.view.backgroundColor = UIColor.blue
+        if peripheral.state == .poweredOn {
+            print("\(#function)")
+            peripheralManager.startAdvertising(beaconPeripheralData as? [String: Any])
+        } else {
+            print("is off")
+        }
+        
     }
+    
+    
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        print("\(error)")
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
+        print("\(error)")
+    }
+    
     
 }
 
